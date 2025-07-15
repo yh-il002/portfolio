131d568b30754bd495b479b7397b012e01457b0b
平松泰明 <yhiramatsu@ntv-wands.co.jp>
2025-07-15 09:20:04 +0900

    Vueファイル内のscss、html保存時の自動整形を追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,33 +8,40 @@ import HelloWorld from './components/HelloWorld.vue'
             <img src="/vite.svg" class="logo" alt="Vite logo" />
         </a>
         <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/images/vue.svg" class="logo vue" alt="Vue logo" />
+            <img
+                src="./assets/images/vue.svg"
+                class="logo vue"
+                alt="Vue logo"
+                width="0"
+                height="0"
+            />
         </a>
     </div>
     <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped lang="scss">
-@use '/src/assets/scss/utility/mq' as *;
-@use '/src/assets/scss/utility/variables' as *;
-@use '/src/assets/scss/common.scss' as *;
+    @use '/src/assets/scss/utility/mq' as *;
+    @use '/src/assets/scss/utility/variables' as *;
+    @use '/src/assets/scss/common.scss' as *;
 
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+    .logo {
+        width: 100px;
+        height: 6em;
+        padding: 1.5em;
+        transition: filter 300ms;
+        will-change: filter;
 
-    @include mq($until: desktop) {
-        height: 10em;
+        @include mq($until: desktop) {
+            height: 10em;
+        }
     }
-}
 
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
+    .logo:hover {
+        filter: drop-shadow(0 0 2em #646cffaa);
+    }
 
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
+    .logo.vue:hover {
+        filter: drop-shadow(0 0 2em #42b883aa);
+    }
 </style>
